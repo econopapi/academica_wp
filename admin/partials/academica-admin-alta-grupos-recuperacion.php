f3382f457ceec3acb6daa4b4fe988052cacad042
Daniel Limón <dlimon.munoz@gmail.com>
2024-05-29 03:07:43 -0600

    Started Admin Area development (on WP Dashboard)

---
+++ admin/partials/academica-admin-alta-grupos-recuperacion.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://example.com
+ * @since      0.1
+ *
+ * @package    Academica
+ * @subpackage Academica/admin/partials
+ */
+
+echo "<h1>Académica UAM - Alta de Grupos (Evaluación de Recuperación)</h1>";
+?>
+
+
+
--- admin/partials/academica-admin-display.php
+++ admin/partials/academica-admin-display.php
@@ -12,7 +12,23 @@
  * @subpackage Academica/admin/partials
  */
 
-echo "<h1>Academica Admin Display</h1>";
+echo "<h1>Academica UAM - Administración de Sistema Académico</h1>";
 ?>
 
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div class="wrap">
+    <h2>Configuración de API</h2>
+    <form method="post">
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">URL de la API</th>
+                <td><input type="text" name="api_url" value="<?php echo get_option('api_url'); ?>" /></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">API Key</th>
+                <td><input type="text" name="api_user" value="<?php echo get_option('api_user'); ?>" /></td>
+            </tr>
+
+        </table>
+        <input type="submit" name="submit" id="submit" class="button button-primary" value="Guardar cambios">
+
+
