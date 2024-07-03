a30ee54c97e71af15028fa9a01b7dd10c3cb165e
Daniel Limón <dlimon.munoz@gmail.com>
2024-07-03 13:15:07 -0600

    Se añade interfaz para alta de grupos para evaluación de recuperaciń

---
+++ admin/js/academica-admin-alta-grupos-recuperacion.js
@@ -0,0 +1,150 @@
+var selectModulo = document.getElementById('modulo');
+var selectGrupo = document.getElementById('grupo');
+var asignacionForm = document.getElementById('asignacion-form');
+
+selectGrupo.disabled = true;
+selectModulo.addEventListener('change', function() {
+
+    var moduloSeleccionado = this.value;
+
+    // Crear la opción de "Cargando..."
+    selectGrupo.innerHTML = '';
+    var loadingOption = document.createElement('option');
+    loadingOption.value = '';
+    loadingOption.text = 'Cargando...';
+    selectGrupo.appendChild(loadingOption);
+
+    fetch(`https://academica.dlimon.net/coordinacion/recuperacion/grupos?modulo=${moduloSeleccionado}`)
+        .then(response => response.json())
+        .then(data => {
+
+            selectGrupo.disabled = false;
+            selectGrupo.innerHTML = '';
+
+            var option = document.createElement('option');
+            option.value = '';
+            option.text = 'Seleccione un grupo';
+            selectGrupo.appendChild(option);
+
+            data.data.forEach(grupo => {
+                var option = document.createElement('option');
+                option.value = grupo;
+                option.text = grupo.toUpperCase();
+                selectGrupo.appendChild(option);
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    asignacionForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        // Info de asignación docente, grupo, módulo y trimestre
+    
+        var modulo = this.modulo.value;
+        var grupo = this.grupo.value;
+        var trimestre = this.trimestre.value;
+        var coordinacion = this.coordinacion.value;
+    
+        var docentes = [
+            {
+                docente: parseInt(this.teoria.value),
+                componente: 'teoria',
+                coordinacion: coordinacion === 'teoria'
+            },
+            {
+                docente: parseInt(this.matematicas.value),
+                componente: 'matematicas',
+                coordinacion: coordinacion === 'matematicas'
+            },
+            {
+                docente: parseInt(this.taller.value),
+                componente: 'taller',
+                coordinacion: coordinacion === 'taller'
+            },
+            {
+                docente: parseInt(this.investigacion.value),
+                componente: 'investigacion',
+                coordinacion: coordinacion === 'investigacion'
+            },       
+        ];
+
+        let frecuenciaDocentes = {};
+
+        docentes.forEach(docente => {
+            if (!frecuenciaDocentes[docente.docente]) {
+                frecuenciaDocentes[docente.docente] = { count: 1, hadCoordination: docente.coordinacion };
+            } else {
+                frecuenciaDocentes[docente.docente].count++;
+                if (docente.coordinacion) {
+                    frecuenciaDocentes[docente.docente].hadCoordination = true;
+                }
+            }
+        });
+
+        let docentesRepeditosCoord = [];
+        for (let docente in frecuenciaDocentes) {
+            if (frecuenciaDocentes[docente].count > 1 && frecuenciaDocentes[docente].hadCoordination) {
+                docentesRepeditosCoord.push(parseInt(docente));
+            }
+        }
+
+        docentes.forEach(docente => {
+            if (docentesRepeditosCoord.includes(docente.docente)) {
+                docente.coordinacion = true;
+            }
+        });
+    
+        //Serialización de archivos XLSX de listas de alumnos
+    
+        var excelFile = document.getElementById('excel_file');
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var data = new Uint8Array(e.target.result);
+            var workbook = XLSX.read(data, {type: 'array'});
+    
+            var result = [];
+            workbook.SheetNames.forEach(sheetName => {
+                var rows = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+                if (rows.length) {
+                    rows = rows.map(row => {
+                        let keys = Object.keys(row);
+                        return {
+                            numero_lista: row['numero_lista'],
+                            matricula: row['matricula'],
+                            nombre: row['nombre_alumno'],
+                            teoria: row['teoria'] ? row['teoria']: null,
+                            matematicas: row['matematicas'] ? row['matematicas']: null,
+                            taller: row['taller'] ? row['taller']: null,
+                            investigacion: row['investigacion'] ? row['investigacion']: null,
+                        }
+                    });
+                    result = result.concat(rows);
+                }
+            });
+    
+            var data = {
+                uea: modulo,
+                grupo: grupo,
+                trimestre: trimestre,
+                docentes: docentes,
+                alumnos: result
+            };
+    
+            console.log(JSON.stringify(data, 2, 2));
+            debugger;
+
+            fetch('https://academica.dlimon.net/coordinacion/global/grupos', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            }).then(response => response.json())
+              .then(data => console.log(data));
+        }
+        reader.readAsArrayBuffer(excelFile.files[0]);    
+    });
+});
+
--- admin/partials/academica-admin-alta-grupos-recuperacion.php
+++ admin/partials/academica-admin-alta-grupos-recuperacion.php
@@ -12,8 +12,118 @@
  * @subpackage Academica/admin/partials
  */
 
-echo "<h1>Académica UAM - Alta de Grupos (Evaluación de Recuperación)</h1>";
+echo "<h1>Académica UAM - Alta de Grupos (Evaluación Global)</h1>";
 ?>
 
+<div class="evaluacion-componente-head">
+<div class="table-2">
+    <form class="search-form-2" id="asignacion-form" enctype="multipart/form-data">       
+        <table class="table-1">     
+            
+            <thead>
+                <tr>
+                    <th>Trimestre</th>
+                    <th>Módulo</th>
+                    <th>Grupo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <select id="trimestre" name="trimestre" disabled>
+                            <option value="24i">24 Invierno</option>
+                        </select>
+                    </td>
+                    <td>
+                        <select id="modulo" name="modulo">}
+                            <option value="">Módulo</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                        </select>
+                    </td>
+                    <td>
+                        <select id="grupo" name="grupo">
+                            <option value="">Grupo</option>
+                            <option value="23o">SD01E</option>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table class="table-1">     
+            
+            <thead>
+                <tr>
+                    <th>Componente</th>
+                    <th>Docente</th>
+                    <th>Coordinación</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            <tr>
+                <td>Teoría</td>
+                <td><input type="text" name="teoria" placeholder="Número económico" style="width: 100%!important; padding: 2px!important;"></td>
+                <td><input type="radio" name ="coordinacion" value="teoria"></td>
+            </tr>
+
+            <tr>
+                <td>Matemáticas</td>
+                <td><input type="text" name="matematicas" placeholder="Número económico" style="width: 100%!important; padding: 2px!important;"></td>
+                <td><input type="radio" name ="coordinacion" value="matematicas"></td>
+            </tr>
+
+            <tr>
+                <td>Taller</td>
+                <td><input type="text" name="taller" placeholder="Número económico" style="width: 100%!important; padding: 2px!important;"></td>
+                <td><input type="radio" name ="coordinacion" value="taller"></td>
+            </tr>   
+            
+            <tr>
+                <td>Investigación</td>
+                <td><input type="text" name="investigacion" placeholder="Número económico" style="width: 100%!important; padding: 2px!important;"></td>
+                <td><input type="radio" name ="coordinacion" value="investigacion"></td>
+            </tr> 
+            </tbody>
+        </table>
+
+        <table class="table-1">
+            <thead>
+                <tr>
+                    <th>
+                        Lista de alumnado
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="center-content">
+                        <label>El archivo XLSX debe contener únicamente tres columnas: número de lista, matrícula y nombre</label>                
+                    </td>
+                </tr>
+                <tr>
+                    <td class="center-content">
+                        <input type="file" id="excel_file" name="excel_file" accept=".xlsx">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+
+        
+        <input type="submit" value="Registrar grupo">
+    </form>
+</div>
+
+<script src="https://unpkg.com/xlsx/dist/xlsx.full.min.js"></script>
+<script src="<?php echo plugins_url('/js/academica-admin-alta-grupos-recuperacion.js', dirname(__FILE__)); ?>"></script>
 
 
